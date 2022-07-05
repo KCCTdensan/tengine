29309a1c018abae61f4914b2ec298beed44044a0
YamaD <me@dyama.net>
2022-07-06 00:59:44 +0900

    meson

---
+++ include/common.hh
@@ -0,0 +1,8 @@
+#include <string>
+
+namespace tengine {
+static inline constexpr std::string_view name{"tengine"};
+static inline constexpr std::string_view version{"0.0"};
+// static inline constexpr std::string_view info =
+//     std::format("{} v{}", name, version);
+} // namespace tengine
